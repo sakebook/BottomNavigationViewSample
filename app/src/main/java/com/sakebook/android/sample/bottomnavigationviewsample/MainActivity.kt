4594fddc59d8e32d5e7df85f5d14b3a45a73883e
Shinya Sakemoto <sakebook@gmail.com>
2017-02-05 16:55:50 +0900

    Add BottomNavigationView

--- app/src/main/java/com/sakebook/android/sample/bottomnavigationviewsample/MainActivity.kt
+++ app/src/main/java/com/sakebook/android/sample/bottomnavigationviewsample/MainActivity.kt
@@ -2,11 +2,41 @@ package com.sakebook.android.sample.bottomnavigationviewsample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val bottomNavigation by lazy { findViewById(R.id.bottom_navigation) as BottomNavigationView }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        val tabCount = 3
+        val fragments = (0 until tabCount).mapIndexed { i, value ->
+            MainFragment.newInstance(i)
+        }
+
+        supportFragmentManager.beginTransaction().replace(R.id.layout_container, fragments[0]).commit()
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            return@setOnNavigationItemSelectedListener when(it.itemId) {
+                R.id.navigation_recents -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.layout_container, fragments[0]).commit()
+                    true
+                }
+                R.id.navigation_favorites -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.layout_container, fragments[1]).commit()
+                    true
+                }
+                R.id.navigation_nearby -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.layout_container, fragments[2]).commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/sakebook/android/sample/bottomnavigationviewsample/MainFragment.kt
@@ -0,0 +1,87 @@
+package com.sakebook.android.sample.bottomnavigationviewsample
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.StaggeredGridLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+
+/**
+ * Created by sakemotoshinya on 2017/02/05.
+ */
+class MainFragment: Fragment() {
+
+    companion object {
+        val ARG_KEY_SEED = "arg_key_seed"
+        @JvmStatic
+        fun newInstance(seed: Int): Fragment {
+            val fragment = MainFragment()
+            val bundle = Bundle()
+            bundle.putInt(ARG_KEY_SEED, seed)
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, null)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerView = view.findViewById(R.id.recycler_view) as RecyclerView
+        val items = (0..100).map { "item: $it" }
+        val args = arguments
+        val seedId = args.getInt(ARG_KEY_SEED)
+        recyclerView.layoutManager = when(seedId) {
+            1 -> GridLayoutManager(context, 3)
+            2 -> StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+            else -> LinearLayoutManager(context)
+        }
+        recyclerView.adapter = MainAdapter(context, items)
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    class MainAdapter(val context: Context, val items: List<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            return ViewHolder(View.inflate(context, R.layout.list_item, null))
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            holder as ViewHolder
+            holder.textView.text = items[position]
+        }
+
+        override fun getItemCount(): Int {
+            return items.count()
+        }
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textView: TextView = itemView.findViewById(R.id.text_view) as TextView
+    }
+}
