b27aabd0a6bd44a3d57ff3415fe7b22631144bfa
Shinya Sakemoto <sakebook@gmail.com>
2017-02-06 00:17:20 +0900

    Change tool bar behavior

--- app/src/main/java/com/sakebook/android/sample/bottomnavigationviewsample/MainActivity.kt
+++ app/src/main/java/com/sakebook/android/sample/bottomnavigationviewsample/MainActivity.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.os.Build
+import android.support.v7.widget.Toolbar
 import android.view.View
 
 
 class MainActivity : AppCompatActivity() {
 
+    private val toolbar by lazy { findViewById(R.id.toolbar) as Toolbar }
     private val bottomNavigation by lazy { findViewById(R.id.bottom_navigation) as BottomNavigationView }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,8 +19,14 @@ class MainActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             findViewById(android.R.id.content).systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         }
+
         setContentView(R.layout.activity_main)
+        initToolbar()
         initBottomNavigation()
+    }
+
+    private fun initToolbar() {
+        toolbar.title = "BottomNavigationViewSample"
     }
 
     private fun initBottomNavigation() {
