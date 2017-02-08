56274d3b2fcb9ed85da320fc919f8bfec3a1d815
Shinya Sakemoto <sakebook@gmail.com>
2017-02-08 13:17:32 +0900

    Add cardview

--- app/src/main/java/com/sakebook/android/sample/bottomnavigationviewsample/MainAdapter.kt
+++ app/src/main/java/com/sakebook/android/sample/bottomnavigationviewsample/MainAdapter.kt
@@ -3,24 +3,39 @@ package com.sakebook.android.sample.bottomnavigationviewsample
 import android.content.Context
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 
 /**
  * Created by sakemotoshinya on 2017/02/06.
  */
 class MainAdapter(val context: Context, val items: List<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return ViewHolder(View.inflate(context, R.layout.list_item, null))
+    val inflater = LayoutInflater.from(context)
+    val SMALL_VIEW_TYPE = 1
+    val LARGE_VIEW_TYPE = 2
+
+    override fun getItemViewType(position: Int): Int {
+        return when(position % 2) {
+            1 -> SMALL_VIEW_TYPE
+            else -> LARGE_VIEW_TYPE
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder? {
+        return when(viewType) {
+            SMALL_VIEW_TYPE -> ViewHolder(inflater.inflate(R.layout.list_item, parent, false))
+            else -> ViewHolder(inflater.inflate(R.layout.list_item_large, parent, false))
+        }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         holder as ViewHolder
         holder.textView.text = items[position]
-        val bgColor = if (position % 2 == 0) R.color.white else R.color.gray
-        holder.itemView.setBackgroundColor(ContextCompat.getColor(context, bgColor))
     }
 
     override fun getItemCount(): Int {
